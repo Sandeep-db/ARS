08a857387b6ca031fc27420a043964c39aed820b
genoos <20jayanth02@gmail.com>
2023-03-12 15:31:31 +0530

    added age

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,47 +1,51 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const addressSchema = new mongoose.Schema({
-    country: {
-        type: String,
-        required: true
-    },
-    state: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    }
-})
+  country: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+});
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true,
-        length: 12
-    },
-    CreatedAt: {
-        type: Date,
-        immutable: true,
-        default: () => Date.now()
-    },
-    passwd: {
-        type: String,
-        required: true
-    },
-    address: addressSchema,
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    length: 12,
+  },
+  Age: {
+    type: Number,
+    required: true,
+  },
+  CreatedAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+  },
+  passwd: {
+    type: String,
+    required: true,
+  },
+  address: addressSchema,
+});
 
-userSchema.index({ email: 1 }, { unique: true })
-userSchema.index({ phone: 1 }, { unique: true })
+userSchema.index({ email: 1 }, { unique: true });
+userSchema.index({ phone: 1 }, { unique: true });
 
-export default mongoose.model("User", userSchema)
+export default mongoose.model("User", userSchema);
